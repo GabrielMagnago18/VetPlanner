4afb6d2f08e9bb9e645fcd1b1909b1e3b81bbde2
Gabriel Batista <gabriel.magnago14@gmail.com>
2025-02-05 18:11:18 -0300

    adicionando o react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/home";
+
 function App() {
   return (
     <>
-      <div>VetPlanner</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
